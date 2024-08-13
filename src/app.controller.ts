0803c0366f51aa3209eef45939f49035df169f01
wdj <dingjie.wang@supwisdom.com>
2024-08-13 17:31:43 +0800

    feat: curd finished

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { UserService } from './modules/user/user.service';
+import { User } from './modules/user/models/user.entity';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly userService: UserService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/create')
+  async create(): Promise<boolean>{
+    return await this.userService.create({
+      name: '水滴超级管理员',
+      desc: '管理员',
+      tel: '8800888',
+      password: '123456',
+      account: 'admin'
+    })
+  }
+
+  @Get('/del')
+  async del(): Promise<boolean>{
+    return await this.userService.del('4475688b-780f-4c73-a022-6222d7d72bed');
+  }
+
+  @Get('/update')
+  async update(): Promise<boolean>{
+    return await this.userService.update(
+      '16196544-9921-4514-9c12-b3f3d163289b',
+      {
+        name: '被修改的名称'
+      }
+    )
+  }
+
+  @Get('/find')
+  async find(): Promise<User>{
+    return await this.userService.find('5f8a1b2d-e82e-497c-9dbc-3db8e749757d')
   }
 }
