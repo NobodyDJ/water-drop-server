12e5c749cc1ae2cdf7eb64580371be260829cdf7
wdj <dingjie.wang@supwisdom.com>
2024-09-05 10:22:01 +0800

    feat: add jwt_secret

--- src/common/constants/aliyun.ts
+++ src/common/constants/aliyun.ts
@@ -2,3 +2,4 @@ export const SIGN_NAME = '水滴';
 export const TEMPLATE_CODE = 'SMS_472115246';
 export const ACCESS_KEY_ID = '';
 export const ACCESS_KEY_SECRET = '';
+export const JWT_SECRET = 'nobody'; // jwt的密钥
