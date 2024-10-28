aa3ca6cc7ce13185ec5c04b8b8eefc5ef0e78ccb
NobodyDJ <108987166+NobodyDJ@users.noreply.github.com>
2024-10-28 17:25:51 +0800

    feat: wechat pay format finished

--- src/modules/wxpay/wxpay.module.ts
+++ src/modules/wxpay/wxpay.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WxpayController } from './wxpay.controller';
 import { StudentModule } from '../student/student.module';
+import { WxpayResolver } from './wxpay.resolver';
+import { ProductModule } from '../product/product.module';
 
 @Module({
   controllers: [WxpayController],
-  imports: [StudentModule],
+  providers: [WxpayResolver],
+  imports: [StudentModule, ProductModule],
 })
 export class WxpayModule {}
